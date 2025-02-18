6f37f27d0d00c9e09141cfa8f3cb74d56a05ab44
Michael Satterfield <michaelsatterfield@Michaels-Mac-mini.local>
2025-02-18 12:58:26 -0600

    added spring

---
+++ resume-backend/src/main/java/com/example/resume_backend/ResumeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.resume_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeBackendApplication.class, args);
+	}
+
+}
