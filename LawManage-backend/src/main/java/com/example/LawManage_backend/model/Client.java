46ab3853a4ab881d046e599fa398f9f5ef28accd
Michael <michael.f.satterfield@gmail.com>
2025-04-25 08:57:05 -0500

    1

--- LawManage-backend/src/main/java/com/example/LawManage_backend/model/Client.java
+++ LawManage-backend/src/main/java/com/example/LawManage_backend/model/Client.java
@@ -34,36 +34,4 @@ public class Client {
      //   @OneToMany(mappedBy = "client") // "client" is the field in Cases
     //    private List<Cases> cases; // List of cases associated with this client
 
-    // Getters and setters not needed with Lombok
-//     public Long getId() {
-//         return id;
-//     }
-
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
-
-//     public String getName() {
-//         return name;
-//     }
-
-//     public void setName(String name) {
-//         this.name = name;
-//     }
-
-//     public String getPosition() {
-//         return position;
-//     }
-
-//     public void setPosition(String position) {
-//         this.position = position;
-//     }
-
-//     public Double getSalary() {
-//         return salary;
-//     }
-
-//     public void setSalary(Double salary) {
-//         this.salary = salary;
-//     }
  }
